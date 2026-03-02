53cbba5ca7ffdf3c82c0f60092e570f9b07cab20
Tomáš Chochola <tomaschochola@tomaschochola.cz>
2026-03-02 20:19:30 +0100

    Initial commit

---
+++ tests/LibTest.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * @author Tomáš Chochola <tomaschochola@tomaschochola.cz>
+ * @copyright © 2026 Tomáš Chochola <tomaschochola@tomaschochola.cz>
+ *
+ * @license CC-BY-ND-4.0
+ *
+ * @see {@link https://creativecommons.org/licenses/by-nd/4.0/} License
+ * @see {@link https://github.com/tomaschochola} GitHub Profile
+ * @see {@link https://github.com/sponsors/tomaschochola} GitHub Sponsors
+ */
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Small;
+use PHPUnit\Framework\Attributes\Test;
+use TomasChochola\Template\Library\Lib;
+
+/**
+ * @internal
+ *
+ * @no-named-arguments
+ */
+#[CoversClass(Lib::class)]
+#[Small]
+final class LibTest extends TestCase
+{
+    #[Test]
+    public function testEncode(): void
+    {
+        $lib = new Lib();
+
+        self::assertSame($lib::class, $lib());
+    }
+}
